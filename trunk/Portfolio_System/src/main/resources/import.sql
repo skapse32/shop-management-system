0ffc3b6fc1a8d0dad8c0940c58a38ff13dc327ef
pnthang01@gmail.com <pnthang01@gmail.com@a0d33425-8d3b-0478-3463-5dfa611fca13>
2013-03-02 04:28:50 +0000

    add source code(3).

---
+++ trunk/Portfolio_System/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+-- You can use this file to load seed data into the database using SQL statements
+insert into Member (member_id, name, email, phone_number) values (0, 'John Pham', 'john.smith@mailinator.com', '2125551212')
+insert into Video  (video_id, code, name, link_value, view_count, creation_date) values (0, 'saRG2fsSF2', 'One Piece 200', 'www.google.com.vn', 0, '2012-12-27')
